21310b44bdb1391f19c1440ec6ecb40750e07227
JadirBrito5485 <brito302022@gmail.com>
2026-04-16 00:06:00 -0300

    Add files via upload

---
+++ conteudo.js
@@ -0,0 +1,33 @@
+const artigos = [
+    {
+        id: "estudo-01",
+        titulo: "Muitos, mas um escolhido",
+        imagemUrl: "https://i.pinimg.com/1200x/60/d2/21/60d221c0263704061d18f837dfde82f3.jpg",
+        texto: `O tema central deste estudo baseia-se na compreensão de que Deus observa a todos, mas age de forma específica em determinados indivíduos para cumprir um propósito que transcende a compreensão humana...`
+    },
+   
+    {
+        id: "mural-eventos",
+        titulo: "PAI E FILHA - JADIR E TÁBITA",
+        imagemUrl: "img/foto01.jpg", // Usei a foto 01 como capa já que a capa-mural deu erro
+        texto: `TE AMO MUITO`,
+        mural: [
+            "img/foto01.jpg",
+            "img/foto02.jpg",
+            "img/foto03.jpg",
+            "img/foto04.jpg",
+            "img/foto05.jpg",
+            "img/foto06.jpg",
+            "img/foto07.jpg",
+            "img/foto08.jpg",
+            "img/foto09.jpg",
+            "img/foto10.jpg",
+            "img/foto11.jpg",
+            "img/foto12.jpg",
+            "img/foto13.jpg",
+            "img/foto14.jpg",
+            "img/foto15.jpg",
+            "img/foto16.jpg"
+        ]
+    }
+];
